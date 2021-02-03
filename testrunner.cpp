388ed93e3ee42fb58b4538cd9d86016f204d8138
Tristan Krause <tristankrause@protonmail.com>
2021-02-03 21:11:15 +0100

    implemented split

--- testrunner.cpp
+++ testrunner.cpp
@@ -12,8 +12,101 @@ int main(int argc, char* argv[])
 	return Catch::Session().run(argc, argv);
 }
 
-TEST_CASE("split")
+TEST_CASE("split - by character - 0")
 {
+	const auto str1 = "";
+	const auto split1 = torquis::split({ str1 }, '#');
+	REQUIRE(split1.size() == 1);
+	CHECK(split1[0] == "");
+}
 
-	REQUIRE(true);
+TEST_CASE("split - by character - 1")
+{
+	const auto str1 = "abc#def#ghi";
+	const auto split1 = torquis::split({ str1 }, '#');
+	REQUIRE(split1.size() == 3);
+	CHECK(split1[0] == "abc");
+	CHECK(split1[1] == "def");
+	CHECK(split1[2] == "ghi");
+}
+
+TEST_CASE("split - by character - 2")
+{
+	const auto str1 = "#abc#def#ghi##";
+	const auto split1 = torquis::split({ str1 }, '#');
+	REQUIRE(split1.size() == 6);
+	CHECK(split1[0] == "");
+	CHECK(split1[1] == "abc");
+	CHECK(split1[2] == "def");
+	CHECK(split1[3] == "ghi");
+	CHECK(split1[4] == "");
+	CHECK(split1[5] == "");
+}
+
+TEST_CASE("split - by character - 3")
+{
+	const auto str1 = "###";
+	const auto split1 = torquis::split({ str1 }, '#');
+	REQUIRE(split1.size() == 4);
+	CHECK(split1[0] == "");
+	CHECK(split1[1] == "");
+	CHECK(split1[2] == "");
+	CHECK(split1[3] == "");
+}
+
+TEST_CASE("split - by string - 0")
+{
+	const auto str1 = "";
+	const auto delim = "-#";
+	const auto split1 = torquis::split({ str1 }, std::string_view(delim));
+	REQUIRE(split1.size() == 1);
+	CHECK(split1[0] == "");
+}
+
+TEST_CASE("split - by string - 1")
+{
+	const auto str1 = "abc";
+	const auto delim = "";
+	const auto split1 = torquis::split({ str1 }, std::string_view(delim));
+	REQUIRE(split1.size() == 3);
+	CHECK(split1[0] == "a");
+	CHECK(split1[1] == "b");
+	CHECK(split1[2] == "c");
+}
+
+TEST_CASE("split - by string - 2")
+{
+	const auto str1 = "a-bc-#de#f-#g-h#i";
+	const auto delim = "-#";
+	const auto split1 = torquis::split({ str1 }, std::string_view(delim));
+	REQUIRE(split1.size() == 3);
+	CHECK(split1[0] == "a-bc");
+	CHECK(split1[1] == "de#f");
+	CHECK(split1[2] == "g-h#i");
+}
+
+TEST_CASE("split - by string - 3")
+{
+	const auto str1 = "-#abc--##def--##ghi-#-#";
+	const auto delim = "-#";
+	const auto split1 = torquis::split({ str1 }, std::string_view(delim));
+	REQUIRE(split1.size() == 6);
+	CHECK(split1[0] == "");
+	CHECK(split1[1] == "abc-");
+	CHECK(split1[2] == "#def-");
+	CHECK(split1[3] == "#ghi");
+	CHECK(split1[4] == "");
+	CHECK(split1[5] == "");
+}
+
+TEST_CASE("split - by string - 4")
+{
+	const auto str1 = "-#-#-#";
+	const auto delim = "-#";
+	const auto split1 = torquis::split({ str1 }, std::string_view(delim));
+	REQUIRE(split1.size() == 4);
+	CHECK(split1[0] == "");
+	CHECK(split1[1] == "");
+	CHECK(split1[2] == "");
+	CHECK(split1[3] == "");
 }
--- torquis.hpp
+++ torquis.hpp
@@ -10,10 +10,56 @@
 
 namespace torquis
 {
-	template<typename _CharT>
-	std::vector<std::basic_string_view<_CharT>> split(std::basic_string_view<_CharT> sv)
+	/**
+	 * \brief splits a string by a single delim character
+	 * \tparam CharT underlying character type of the string
+	 * \param sv input string
+	 * \param delim separator character
+	 */
+	template<typename CharT>
+	std::vector<std::basic_string_view<CharT>> split(std::basic_string_view<CharT> sv, CharT delim)
 	{
-		return {};
+		std::vector<std::basic_string_view<CharT>> parts;
+		std::size_t prev = 0, curr;
+		do
+		{
+			curr = sv.find(delim, prev);
+			if (curr == std::string::npos) { parts.push_back(sv.substr(prev)); }
+			else { parts.push_back(sv.substr(prev, curr - prev)); }
+			prev = curr + 1;
+		} while (curr != std::string::npos);
+		return parts;
+	}
+
+	/**
+	 * \brief splits a string by a delim string
+	 * \tparam CharT underlying character type of the string
+	 * \param sv input string
+	 * \param delim separator character
+	 */
+	template<typename CharT>
+	std::vector<std::basic_string_view<CharT>> split(std::basic_string_view<CharT> sv, std::basic_string_view<CharT> delim)
+	{
+		std::vector<std::basic_string_view<CharT>> parts;
+		if (delim.length())
+		{
+			std::size_t prev = 0, curr;
+			do
+			{
+				curr = sv.find(delim, prev);
+				if (curr == std::string::npos) { parts.push_back(sv.substr(prev)); }
+				else { parts.push_back(sv.substr(prev, curr - prev)); }
+				prev = curr + delim.length();
+			} while (curr != std::string::npos && prev <= sv.length());
+		}
+		else
+		{
+			for (std::size_t i = 0; i < sv.length(); i++)
+			{
+				parts.push_back(sv.substr(i, 1));
+			}
+		}
+		return parts;
 	}
 }
 
