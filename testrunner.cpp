c531a4d8e1e80f87971b08456f0953d8f57d46bd
Tristan Krause <tristankrause@protonmail.com>
2021-02-03 18:52:47 +0100

    project setup

---
+++ testrunner.cpp
@@ -0,0 +1,19 @@
+//
+// Created by core on 03/02/2021.
+//
+
+#define CATCH_CONFIG_EXTERNAL_INTERFACES
+#define CATCH_CONFIG_RUNNER
+#include <catch2/catch.hpp>
+#include <torquis.hpp>
+
+int main(int argc, char* argv[])
+{
+	return Catch::Session().run(argc, argv);
+}
+
+TEST_CASE("split")
+{
+
+	REQUIRE(true);
+}
---
+++ torquis.hpp
@@ -0,0 +1,20 @@
+//
+// Created by core on 03/02/2021.
+//
+
+#ifndef TORQUIS_HPP
+#define TORQUIS_HPP
+
+#include <string>
+#include <string_view>
+
+namespace torquis
+{
+	template<typename _CharT>
+	std::vector<std::basic_string_view<_CharT>> split(std::basic_string_view<_CharT> sv)
+	{
+		return {};
+	}
+}
+
+#endif //TORQUIS_HPP
