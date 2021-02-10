352a0b4ffdd16704adfa9ab82c8fba5b1cc7cffa
Tristan Krause <tristankrause@protonmail.com>
2021-02-10 22:04:34 +0100

    added toupper

--- include/torquis.hpp
+++ include/torquis.hpp
@@ -299,6 +299,73 @@ namespace torquis
 		tolower(copy);
 		return copy;
 	}
+
+	/*
+	 * function group: toupper
+	 */
+
+	/**
+	 * \brief converts the ascii-characters of a string to their upper case inplace
+	 * \tparam CharT underlying character type of the string
+	 * \param str input string
+	 * \param len input string length
+	 */
+	template<typename CharT>
+	void toupper(CharT* str, std::size_t len)
+	{
+		static_assert(!std::is_const_v<CharT>, "no const type allowed");
+		for (; len--; str++) { *str = std::toupper(*str); }
+	}
+
+	/**
+	 * \brief converts the ascii-characters of a string to their upper case inplace
+	 * \tparam CharT underlying character type of the string
+	 * \param str input string
+	 */
+	template<typename CharT>
+	void toupper(std::basic_string<CharT>& str)
+	{
+		if (!str.empty()) { toupper(&str[0], str.length()); }
+	}
+
+	/**
+	 * \brief converts the ascii-characters of a string to their upper case and returns it
+	 * \tparam CharT underlying character type of the string
+	 * \param str input string
+	 */
+	template<typename CharT>
+	std::basic_string<CharT> toupperc(const std::basic_string_view<CharT> str)
+	{
+		std::basic_string<CharT> copy(str);
+		toupper(copy);
+		return copy;
+	}
+
+	/**
+	 * \brief converts the ascii-characters of a string to their upper case and returns it
+	 * \tparam CharT underlying character type of the string
+	 * \param str input string
+	 */
+	template<typename CharT>
+	std::basic_string<CharT> toupperc(const std::basic_string<CharT>& str)
+	{
+		std::basic_string<CharT> copy(str);
+		toupper(copy);
+		return copy;
+	}
+
+	/**
+	 * \brief converts the ascii-characters of a string to their upper case and returns it
+	 * \tparam CharT underlying character type of the string
+	 * \param str input string
+	 */
+	template<typename CharT>
+	std::basic_string<CharT> toupperc(const CharT* str)
+	{
+		std::basic_string<CharT> copy(str);
+		toupper(copy);
+		return copy;
+	}
 }
 
 #endif //TORQUIS_HPP
---
+++ test/test_toupper.cpp
@@ -0,0 +1,102 @@
+//
+// Created by core on 10/02/2021.
+//
+
+#include <catch2/catch.hpp>
+#include <include/torquis.hpp>
+#include <cstring>
+
+
+/*
+ * tests for character type: char
+ */
+
+TEST_CASE("toupper/char/by-cstring/0")
+{
+	std::string str;
+	torquis::toupper(str); // call string type: string
+	CHECK(str.empty());
+}
+
+TEST_CASE("toupper/char/by-cstring/1")
+{
+	std::string str("aöB?Äk#+ßl");
+	torquis::toupper(str); // call string type: string
+	CHECK(std::string("AöB?ÄK#+ßL") == str);
+}
+
+TEST_CASE("toupper/char/by-cstring/2")
+{
+	std::string str("aöB?Äk#+ßl");
+	torquis::toupper(&str[0], str.length()); // call string type: c-string
+	CHECK(std::string("AöB?ÄK#+ßL") == str);
+}
+
+TEST_CASE("toupper/char/by-cstring/3")
+{
+	std::string_view str("aöB?Äk#+ßl");
+	auto lower = torquis::toupperc(str); // call string type: string_view
+	CHECK(lower == "AöB?ÄK#+ßL");
+}
+
+TEST_CASE("toupper/char/by-cstring/4")
+{
+	std::string str("aöB?Äk#+ßl");
+	auto lower = torquis::toupperc(str); // call string type: string
+	CHECK(lower == "AöB?ÄK#+ßL");
+}
+
+TEST_CASE("toupper/char/by-cstring/5")
+{
+	const char* str = "aöB?Äk#+ßl";
+	auto lower = torquis::toupperc(str); // call string type: c-string
+	CHECK(lower == "AöB?ÄK#+ßL");
+}
+
+
+
+/*
+ * tests for character type: char
+ */
+
+TEST_CASE("toupper/wchar_t/by-cstring/0")
+{
+	std::wstring str;
+	torquis::toupper(str); // call string type: string
+	CHECK(str.empty());
+}
+
+TEST_CASE("toupper/wchar_t/by-cstring/1")
+{
+	std::wstring str(L"aöB?Äk#+ßl");
+	torquis::toupper(str); // call string type: string
+	CHECK(std::wstring(L"AöB?ÄK#+ßL") == str);
+}
+
+TEST_CASE("toupper/wchar_t/by-cstring/2")
+{
+	std::wstring str(L"aöB?Äk#+ßl");
+	torquis::toupper(&str[0], str.length()); // call string type: c-string
+	CHECK(std::wstring(L"AöB?ÄK#+ßL") == str);
+}
+
+TEST_CASE("toupper/wchar_t/by-cstring/3")
+{
+	std::wstring_view str(L"aöB?Äk#+ßl");
+	auto lower = torquis::toupperc(str); // call string type: string_view
+	CHECK(lower == L"AöB?ÄK#+ßL");
+}
+
+TEST_CASE("toupper/wchar_t/by-cstring/4")
+{
+	std::wstring str(L"aöB?Äk#+ßl");
+	auto lower = torquis::toupperc(str); // call string type: string
+	CHECK(lower == L"AöB?ÄK#+ßL");
+}
+
+TEST_CASE("toupper/wchar_t/by-cstring/5")
+{
+	const wchar_t* str = L"aöB?Äk#+ßl";
+	auto lower = torquis::toupperc(str); // call string type: c-string
+	CHECK(lower == L"AöB?ÄK#+ßL");
+}
